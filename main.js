938dcabe82e4d0286441c2b1b817f952860d02c4
unknown <=>
2020-11-23 20:40:15 -0500

    Jquery_23_11_2020

---
+++ main.js
@@ -0,0 +1,36 @@
+let nombre="Richard Alexander";
+let apellido="Briceño Santiago"
+let edad= 35;
+let genero= "Masculino";
+let camisa= "camiseta azul";
+let correo="gsusxto@gmail.com";
+let pantaloneta= "algodon negra con gris";
+let pies_lleva_calzado=true;
+let pelo="crespo negro";
+let color_ojos="negro";
+let altura=1.80;
+let peso=89;
+
+console.log("mi nombre es: "+nombre);
+console.log("mi apellido es: "+apellido);
+console.log("mi edad es: "+edad);
+console.log("Sexo: "+genero);
+console.log("color de camiseta: "+camisa);
+console.log("mi correo es: "+correo);
+console.log("mi pantaloneta es: "+pantaloneta);
+console.log("No lleva calzado: "+pies_lleva_calzado);
+console.log("color de pelo es: "+pelo);
+console.log("Color de ojos es: "+color_ojos);
+console.log("mi altura es: ",altura,"metros");
+console.log("mi peso es: ",peso,"kg");
+
+
+
+
+
+
+
+
+
+
+
